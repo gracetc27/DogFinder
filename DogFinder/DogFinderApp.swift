9a6e3edf07e8c756399c17caa605d903fa51b168
Grace Couch <gracetcouch@gmail.com>
2024-04-24 14:54:31 +0100

    Initial Commit

---
+++ DogFinder/DogFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinderApp.swift
+//  DogFinder
+//
+//  Created by Grace couch on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
