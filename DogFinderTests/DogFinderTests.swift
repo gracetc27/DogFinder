b187bcc5566a21f17bf17d019d505bf4a2429ed1
Grace Couch <gracetcouch@gmail.com>
2025-04-25 16:51:27 +0100

    fake service and more tests

--- DogFinderTests/DogFinderTests.swift
+++ DogFinderTests/DogFinderTests.swift
@@ -9,8 +9,8 @@ import Testing
 @testable import DogFinder
 import Foundation
 
-@Test func successBreedsAssignedFromFetchBreeds() async {
-    let sut = DogViewModel(service: FakeDogService())
+@Test func breedsUpdatedFromFetchBreedsSuccess() async {
+    let sut = DogViewModel(service: FakeDogServiceSuccess())
     #expect(sut.breeds == [])
 
     await sut.fetchBreeds()
@@ -20,19 +20,53 @@ import Foundation
     #expect(sut.breeds[0].bredFor == "fighting")
 }
 
-class FakeDogService: DogService {
-    func fetchBreeds() async -> [DogFinder.BreedInfo] {
-        let breeds = [
+@Test func breedsUpdatedFromFetchBreedsFailure() async {
+    let sut = DogViewModel(service: FakeDogServiceFailure())
+    #expect(sut.breeds == [])
+    
+    await sut.fetchBreeds()
+    
+    #expect(sut.breeds == [])
+}
+
+@Test func fetchImagesResponse() async {
+    let sut = DogViewModel(service: FakeDogServiceSuccess())
+    let id = 3
+
+    let sutResult = await sut.loadImages(id)
+    let serviceResult = [
+        DogImage(id: "1", url: URL(string: "google.com")!)
+    ]
+
+    #expect(sutResult == serviceResult)
+
+}
+class FakeDogServiceSuccess: DogService {
+    func fetchBreeds() async throws -> [BreedInfo] {
+        [
             BreedInfo(id: 0, name: "Staffy", lifeSpan: "13", temperament: "Dopey", referenceImageId: "0", image: DogImage(id: "0", url: URL(string: "google.com")!), height: MeasurementSystem(imperial: "10", metric: "10"), weight: MeasurementSystem(imperial: "10", metric: "10"), countryCode: "UK", description: "Lovely dog", history: "sad history of fighting", bredFor: "fighting", breedGroup: "Terrier", isFavourite: true, onIsFavouritedChanged: { _ in })
         ]
-        return breeds
     }
     
-    func loadImages(id: Int?) async -> [DogFinder.DogImage] {
-        let dogImages = [
+    func loadImages(id: Int?) async throws -> [DogImage] {
+        [
             DogImage(id: "1", url: URL(string: "google.com")!)
         ]
-        return dogImages
+    }
+    
+    func saveFavouriteBreeds() async throws {}
+    
+    func loadFavouriteBreeds() async throws {}
+
+}
+
+class FakeDogServiceFailure: DogService {
+    func fetchBreeds() async throws -> [BreedInfo] {
+        throw CancellationError()
+    }
+    
+    func loadImages(id: Int?) async throws -> [DogImage] {
+        throw CancellationError()
     }
     
     func saveFavouriteBreeds() async throws {}
